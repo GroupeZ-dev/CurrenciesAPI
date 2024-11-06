956bb0c52d902cc277da010a7ceadb4ec0cd21e4
Traqueur-dev <traqueur.dev@gmail.com>
2024-11-06 14:43:48 +0100

    fix gitignore

--- build/docs/javadoc/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"fr.traqueur.currencies","c":"Currencies","l":"deposit(double)"},{"p":"fr.traqueur.currencies","c":"Currencies","l":"valueOf(String)","u":"valueOf(java.lang.String)"},{"p":"fr.traqueur.currencies","c":"Currencies","l":"values()"}];updateSearchResults();
--- build/docs/javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"fr.traqueur.currencies","l":"Currencies"}];updateSearchResults();
