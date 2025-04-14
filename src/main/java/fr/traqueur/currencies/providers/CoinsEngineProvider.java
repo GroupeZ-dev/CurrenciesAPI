6a609a9d79521466f4840b4a23bbd28895f00399
Maxlego08 <maxlego08@sfr.fr>
2025-04-14 15:50:41 +0200

    :memo: Fix coins engine

--- src/main/java/fr/traqueur/currencies/providers/CoinsEngineProvider.java
+++ src/main/java/fr/traqueur/currencies/providers/CoinsEngineProvider.java
@@ -3,30 +3,29 @@ package fr.traqueur.currencies.providers;
 import fr.traqueur.currencies.CurrencyProvider;
 import org.bukkit.OfflinePlayer;
 import su.nightexpress.coinsengine.api.CoinsEngineAPI;
-import su.nightexpress.coinsengine.api.currency.Currency;
 
 import java.math.BigDecimal;
 
 public class CoinsEngineProvider implements CurrencyProvider {
 
-    private final Currency currency;
+    private final String currencyName;
 
     public CoinsEngineProvider(String currencyName) {
-        this.currency = CoinsEngineAPI.getCurrency(currencyName);
+        this.currencyName = currencyName;
     }
 
     @Override
     public void deposit(OfflinePlayer offlinePlayer, BigDecimal amount, String reason) {
-        CoinsEngineAPI.addBalance(offlinePlayer.getUniqueId(), this.currency, amount.doubleValue());
+        CoinsEngineAPI.addBalance(offlinePlayer.getUniqueId(), this.currencyName, amount.doubleValue());
     }
 
     @Override
     public void withdraw(OfflinePlayer offlinePlayer, BigDecimal amount, String reason) {
-        CoinsEngineAPI.removeBalance(offlinePlayer.getUniqueId(), this.currency, amount.doubleValue());
+        CoinsEngineAPI.removeBalance(offlinePlayer.getUniqueId(), this.currencyName, amount.doubleValue());
     }
 
     @Override
     public BigDecimal getBalance(OfflinePlayer offlinePlayer) {
-        return BigDecimal.valueOf(CoinsEngineAPI.getBalance(offlinePlayer.getUniqueId(), this.currency));
+        return BigDecimal.valueOf(CoinsEngineAPI.getBalance(offlinePlayer.getUniqueId(), this.currencyName));
     }
 }
