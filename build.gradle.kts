ea09dea5223a20c7c9dd91b74d029e4ca227c217
Maxlego08 <maxlego08@sfr.fr>
2025-06-21 21:59:06 +0200

    :memo: Add build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,21 @@
+import java.util.Locale
+
 plugins {
-    java
+    `java-library`
+    id("com.github.johnrengelman.shadow") version "7.1.2" // Pour remplacer maven-shade-plugin
     `maven-publish`
 }
 
-group = "fr.traqueur"
+rootProject.extra.properties["sha"]?.let { sha ->
+    version = sha
+}
+
+group = "fr.traqueur.currencies"
 version = property("version") as String
+
+extra.set("targetFolder", file("target/"))
+extra.set("classifier", System.getProperty("archive.classifier"))
+extra.set("sha", System.getProperty("github.sha"))
 
 repositories {
     mavenCentral()
@@ -72,14 +83,54 @@ val generateVersionProperties by tasks.registering {
     }
 }
 
-tasks.named("build") {
-    dependsOn(generateVersionProperties)
+tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
+    archiveClassifier.set("") // Écrase le JAR de base par le fat jar
+}
+
+tasks.build {
+    dependsOn(tasks.shadowJar)
+}
+
+tasks.withType<Jar> {
+    manifest {
+        attributes["Implementation-Title"] = "CurrenciesAPI"
+        attributes["Implementation-Version"] = project.version
+    }
 }
 
 publishing {
+    var repository = System.getProperty("repository.name", "snapshots").replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
+
+    repositories {
+        maven {
+            name = "groupez${repository}"
+            url = uri("https://repo.groupez.dev/${repository.lowercase()}")
+            credentials {
+                username = findProperty("${name}Username") as String? ?: System.getenv("MAVEN_USERNAME")
+                password = findProperty("${name}Password") as String? ?: System.getenv("MAVEN_PASSWORD")
+            }
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+        }
+    }
+
     publications {
-        create<MavenPublication>("maven") {
-            from(components["java"])
+        register<MavenPublication>("groupez${repository}") {
+            pom {
+                groupId = project.group as String?
+                artifactId = rootProject.name.lowercase()
+                version = project.version as String?
+
+                scm {
+                    connection = "scm:git:git://github.com/GroupeZ-dev/${rootProject.name}.git"
+                    developerConnection = "scm:git:ssh://github.com/GroupeZ-dev/${rootProject.name}.git"
+                    url = "https://github.com/GroupeZ-dev/${rootProject.name}/"
+                }
+            }
+            artifact(tasks.shadowJar)
+            // artifact(tasks.javadocJar)
+            // artifact(tasks.sourcesJar)
         }
     }
 }
